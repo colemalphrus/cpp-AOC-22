44d8e91b45d616867b710b822aa15686906a253e
colemalphrus <colemalphrus@gmail.com>
2023-07-31 23:24:20 -0400

    day3 part 1

---
+++ Day3/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+
+using namespace std;
+int main(){
+    cout << "Hello World" << endl;
+    fstream file("./Day3/data.txt");
+    string line;
+
+    int sum = 0;
+    while( getline(file, line) ){
+        string line1 = line.substr(0, line.length()/2);
+        string line2 = line.substr(line.length()/2);
+        set<char> characters(line1.begin(), line1.end());
+
+        for (auto ch: line2) {
+            if (characters.find(ch) != characters.end()) {
+                if (isupper(ch)) {
+                    sum += static_cast<int>(ch) - 'A' + 27;
+                } else {
+                    sum += static_cast<int>(ch) - 'a' + 1;
+                }
+                break;
+            };
+        }
+    };
+
+
+    cout << sum << endl;
+
+
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -8,3 +8,8 @@ run2:
 	g++ -std=c++20 -arch arm64 ./Day2/main.cpp -o output_executable
 	./output_executable
 	rm ./output_executable
+
+run3:
+	g++ -std=c++20 -arch arm64 ./Day3/main.cpp -o output_executable
+	./output_executable
+	rm ./output_executable
